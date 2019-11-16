d34be1ae8a484fa5098015650cc92f0324a2157a
Mehul Gohil <mehul.gohil0810@gmail.com>
2019-11-16 10:02:39 +0530

    fix: add and load admin actions file

--- wp-distraction-free-view.php
+++ wp-distraction-free-view.php
@@ -2,26 +2,26 @@
 /**
  * Plugin Name: WP Distraction Free View
  * Plugin URI: https://wordpress.org/plugins/wp-distraction-free-view/
- * Description: "WP Distraction Free View" plugin provides distraction free viewing mode to the users of the website/blog. 
+ * Description: "WP Distraction Free View" plugin provides distraction free viewing mode to the users of the website/blog.
  * Version: 1.4.2
  * Author: Mehul Gohil
  * Author URI: http://mehulgohil.in/
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: wpdfv
- * 
+ *
  * WP Distraction Free View is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by the Free 
+ * it under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 2 of the License, or any later version.
- * 
+ *
  * WP Distraction Free View is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * WP Distraction Free View. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
- * 
+ *
  * @package    WordPress
  * @subpackage WP Distraction Free View
  * @author     Mehul Gohil <hello@mehulgohil.com>
@@ -154,6 +154,7 @@ if ( ! class_exists( 'WPDFV' ) ) {
         public function includes() {
 
             if ( is_admin() ) {
+		        require_once WPDFV_PLUGIN_DIR . 'includes/admin/admin-actions.php';
 		        require_once WPDFV_PLUGIN_DIR . 'includes/admin/class.settings.php';
 	        }
 
