898d47b56de430ac9296724d346a232c405ae32f
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-03-10 00:06:05 +0530

    add admin filters and actions

---
+++ src/Admin/Actions.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * WP Distraction Free View | Admin Actions.
+ *
+ * @package WordPress
+ * @subpackage WP Distraction Free View
+ * @since 1.0.0
+ */
+
+namespace WPDFV\Admin;
+
+// Bailout, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Actions {
+	/**
+	 * Constructor.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+
+	}
+}
