5ce99b815f34130b96aa7ae8afbf81b23b1d0c7a
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-03-10 01:12:10 +0530

    resolve webpack production issue

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,6 @@ const MiniCSSExtractPlugin = require( 'mini-css-extract-plugin' );
 const ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
 const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
 const WebpackRTLPlugin = require( 'webpack-rtl-plugin' );
-const UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
 const wpPot = require( 'wp-pot' );
 
 const inProduction = ( 'production' === process.env.NODE_ENV );
@@ -15,7 +14,6 @@ const mode = inProduction ? 'production' : 'development';
 
 const config = {
 	mode,
-
 	entry: {
 		wpdfv: [ './assets/src/css/frontend/wpdfv.scss', './assets/src/js/frontend/wpdfv.js' ],
 		'wpdfv-admin': [ './assets/src/css/admin/wpdfv-admin.scss', './assets/src/js/admin/wpdfv-admin.js' ],
@@ -24,25 +22,13 @@ const config = {
 		path: path.join( __dirname, './assets/dist/' ),
 		filename: 'js/[name].js',
 	},
-
-	// Ensure modules like magnific know jQuery is external (loaded via WP).
 	externals: {
 		$: 'jQuery',
 		jquery: 'jQuery',
 		lodash: 'lodash',
 	},
-	devtool: ! inProduction ? 'source-map' : '',
-	optimization: {
-		minimizer: [
-			new UglifyJsPlugin( {
-				test: /\.js(\?.*)?$/i,
-				sourceMap: true,
-			} ),
-		],
-	},
 	module: {
 		rules: [
-
 			// Create RTL styles.
 			{
 				test: /\.css$/,
