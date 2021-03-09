6a8513b3f00442ace785595b3894c647ac92d056
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-03-10 00:04:48 +0530

    add empty actions and filters file

---
+++ src/Includes/Actions.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * WP Distraction Free View | Frontend Actions.
+ *
+ * @package WordPress
+ * @subpackage WP Distraction Free View
+ * @since 1.0.0
+ */
+
+namespace WPDFV\Includes;
+
+// Bailout, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Actions {
+	/**
+	 * Constructor.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+
+	}
+}
---
+++ src/Includes/Filters.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * WP Distraction Free View | Frontend Filters.
+ *
+ * @package WordPress
+ * @subpackage WP Distraction Free View
+ * @since 1.0.0
+ */
+
+namespace WPDFV\Includes;
+
+// Bailout, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Filters {
+	/**
+	 * Constructor.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+
+	}
+}
