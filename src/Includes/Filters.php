3560f472e164d84e9b3d5da66536ce236106edf2
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-03-10 00:16:29 +0530

    migrate frontend filters

--- src/Includes/Filters.php
+++ src/Includes/Filters.php
@@ -24,6 +24,37 @@ class Filters {
 	 * @return void
 	 */
 	public function __construct() {
+		add_filter( 'the_content', [ $this, 'filter_content' ] );
+	}
 
+	/**
+	 * This function is used to filter the content to display distraction free view button.
+	 *
+	 * @param mixed $content Content.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return mixed
+	 */
+	public function filter_content( $content ) {
+		global $post;
+
+		$new_content    = '';
+		$display_btn_at = wpdfv_display_read_mode_btn_at();
+		$button_html    = wpdfv_display_read_mode_button( $post->ID );
+
+		// Bailout, if the display button at setting is disabled.
+		if ( 'disable' === $display_btn_at ) {
+			return $content;
+		} elseif ( 'before_content' === $display_btn_at ) {
+			$new_content .= $button_html;
+			$new_content .= $content;
+		} elseif ( 'after_content' === $display_btn_at ) {
+			$new_content .= $content;
+			$new_content .= $button_html;
+		}
+
+		return $new_content;
 	}
 }
