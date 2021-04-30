7c614395ccfa4d941f463472ac0d90fd052df9b0
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-05-01 00:47:35 +0530

    add settings page header

--- src/Admin/Settings.php
+++ src/Admin/Settings.php
@@ -56,6 +56,7 @@ class Settings extends SettingsApi {
 
 		// Admin Menu.
 		add_action( 'admin_menu', [ $this, 'add_admin_menu' ], 9 );
+		add_action( 'in_admin_header', [ $this, 'render_settings_page_header' ] );
 
 	}
 
@@ -69,13 +70,80 @@ class Settings extends SettingsApi {
 	 */
 	public function add_admin_menu() {
 		add_options_page(
-			__( 'Distraction Free Mode', 'wpdfv' ),
-			__( 'Distraction Free Mode', 'wpdfv' ),
+			esc_html__( 'WP Distraction Free View', 'wpdfv' ),
+			esc_html__( 'Distraction Free Mode', 'wpdfv' ),
 			'manage_options',
-			'wp_distraction_free_view',
+			'wpdfv_settings',
 			[ $this, 'settings_page' ]
 		);
+	}
 
+	/**
+	 * Render Settings Page Header.
+	 *
+	 * @since  1.6.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function render_settings_page_header() {
+		?>
+		<div class="wpdfv-dashboard-header">
+			<div class="wpdfv-dashboard-header-title">
+				<h1>
+					<?php echo esc_html( get_admin_page_title() ); ?>
+				</h1>
+			</div>
+			<?php $this->render_header_navigation(); ?>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Render Header Navigation.
+	 *
+	 * @since  1.6.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function render_header_navigation() {
+		$screen      = get_current_screen();
+		$current_tab = ! empty( $_GET['tab'] ) ? $_GET['tab'] : '';
+		$tabs        = apply_filters(
+			'wpdfv_settings_navigation_tabs',
+			[
+				'settings'      => [
+					'name'  => esc_html__( 'Settings', 'wpdfv' ),
+					'url'   => admin_url( 'admin.php?page=wpdfv_settings' ),
+					'class' => 'settings_page_wpdfv_settings' === $screen->id && '' === $current_tab ? 'active' : '',
+				],
+				'other-plugins' => [
+					'name'  => esc_html__( 'Recommended Plugins', 'wpdfv' ),
+					'url'   => admin_url( 'admin.php?page=wpdfv_settings&tab=recommended_plugins' ),
+					'class' => 'settings_page_wpdfv_settings' === $screen->id && 'recommended_plugins' === $current_tab ? 'active' : '',
+				],
+			],
+		);
+
+		// Don't print any markup if we only have one tab.
+		if ( count( $tabs ) === 1 ) {
+			return;
+		}
+		?>
+		<div class="wpdfv-header-navigation">
+			<?php
+			foreach ( $tabs as $tab ) {
+				printf(
+					'<a href="%1$s" class="%2$s">%3$s</a>',
+					esc_url( $tab['url'] ),
+					esc_attr( $tab['class'] ),
+					esc_html( $tab['name'] )
+				);
+			}
+			?>
+		</div>
+		<?php
 	}
 
 	/**
