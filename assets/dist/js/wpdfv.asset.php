6bfc75e919f037de40759fa04662e47b946426ee
Mehul Gohil <hello@mehulgohil.com>
2026-05-12 18:50:15 +0530

    feat: make reader modal full width

--- assets/dist/js/wpdfv.asset.php
+++ assets/dist/js/wpdfv.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1fed0e60603a6464bc14');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '21731074c643f12f4d92');
