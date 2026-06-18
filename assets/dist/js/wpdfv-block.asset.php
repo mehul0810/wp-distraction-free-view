59f21cdec6fda9aa7b167368d4ee66b66c7a898f
Mehul Gohil <hello@mehulgohil.com>
2026-06-18 10:08:54 +0530

    Rebuild reader block assets

--- assets/dist/js/wpdfv-block.asset.php
+++ assets/dist/js/wpdfv-block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0f9ff50784c0cef2d4df');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '380a131a07a6dc696386');
