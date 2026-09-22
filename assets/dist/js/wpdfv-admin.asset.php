d9ff517140f886f1e6c0ce3cc2938dae8e403e39
Mehul Gohil <hello@mehulgohil.com>
2026-09-23 00:37:53 +0530

    Redesign admin settings for 1.9.0

--- assets/dist/js/wpdfv-admin.asset.php
+++ assets/dist/js/wpdfv-admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c5549303f1c190a1e61e');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b4ce853ed9ac5d471209');
